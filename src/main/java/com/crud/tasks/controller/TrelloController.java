5aeaf6666202f6b66155c3f0f440f7fd73a97f6d
Jacobanna <pieprzyk.jakub@gmail.com>
2018-07-26 22:31:03 +0200

    Creating endpoint to export tasks (18.4)

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -19,18 +19,25 @@ public class TrelloController {
     @Autowired
     private TrelloClient trelloClient;
 
+//    1) USING sout()
+//    @RequestMapping(method = RequestMethod.GET, value = "getTrelloBoards")
+//    public void getTrelloBoards() {
+//
+//        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
+//
+//        for (TrelloBoardDto trelloBoardDto: trelloBoards) {
+//            if(trelloBoardDto.getName() != null && trelloBoardDto.getId() != null && trelloBoardDto.getName().contains("Kodilla")) {
+//                System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName());
+//                System.out.println("This board contains lists: ");
+//                trelloBoardDto.getLists().forEach(trelloList -> System.out.println(trelloList.getName() + " " + trelloList.getId() + " " + trelloList.isClosed()));
+//            }
+//        }
+//    }
+
+//    2) Using Logger
     @RequestMapping(method = RequestMethod.GET, value = "getTrelloBoards")
-    public void getTrelloBoards() {
-
-        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
-
-        for (TrelloBoardDto trelloBoardDto: trelloBoards) {
-            if(trelloBoardDto.getName() != null && trelloBoardDto.getId() != null && trelloBoardDto.getName().contains("Kodilla")) {
-                System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName());
-                System.out.println("This board contains lists: ");
-                trelloBoardDto.getLists().forEach(trelloList -> System.out.println(trelloList.getName() + " " + trelloList.getId() + " " + trelloList.isClosed()));
-            }
-        }
+    public List<TrelloBoardDto> getTrelloBoards() {
+        return trelloClient.getTrelloBoards();
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "createTrelloCard")
